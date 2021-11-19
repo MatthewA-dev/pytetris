c8b940be78c2e3e8bdee2e064e56b2dfd6844b61
MatthewA-dev <aleshechkin.matthew@gmail.com>
2021-11-18 23:03:39 -0600

    Improved AI

--- main.py
+++ main.py
@@ -28,21 +28,21 @@ class AI():
     bestmove = (math.inf, [0,0], piece) 
     #moves = []
     for _ in range(4):
-      pc = self.board.defaultPieceCoords
+      pc = self.board.getPieceCoords(piece)
       #pc = [0,0]
     # get the furthest to the left coord that's valid for every piece rotation
-      while(not self.board.checkOut(piece, pc)):
+      while(not (self.board.checkOut(piece, pc) or self.board.checkOverlapping(piece,pc,self.board.grid))):
         pc = [pc[0] - 1, pc[1]]
       pc = [pc[0] + 1, pc[1]]
       # Try every possible place for piece
-      while not self.board.checkOut(piece, pc):
+      while not (self.board.checkOut(piece, pc) or self.board.checkOverlapping(piece,pc,self.board.grid)):
         # Hard drop the piece
         piececoords = copy.deepcopy(pc)
-        while not self.board.checkOverlapping(piece,piececoords) and not self.board.checkInGround(piece, piececoords):
+        while not self.board.checkOverlapping(piece,piececoords, self.board.grid) and not self.board.checkInGround(piece, piececoords):
           piececoords = (piececoords[0], piececoords[1] + 1)
         piececoords = (piececoords[0], piececoords[1] - 1)
 
-        score = self.evaluate(self.board.placePiece(True, piece, piececoords))
+        score = self.evaluate(self.board.placePiece(True, piece, piececoords), False)
         #moves.append((score,(pc,piece)))
         if(score < bestmove[0]):
           bestmove = (score, pc, piece)
@@ -55,82 +55,95 @@ class AI():
     #return (0,moves[0][1][0],moves[0][1][1])
     return bestmove
       
-  def evaluate(self,grid):
-    heights = []
-    for x in range(len(grid[0])):
-      col = []
-      for row in grid:
-        col.append(row[x])
-      height = 0
-      for y in col:
-          if(y == 0):
-              height += 1
-          else:
-              break
-      heights.append(len(col) - height)
-    d = 0
-    for i, h in enumerate(heights):
-      try:
-        if(i == 0):
+  # only get the evaluation of the board, ignoring next piece
+  def evaluate(self, grid, onlyGrade):
+    def getBoardDif(grid):
+      heights = []
+      for x in range(len(grid[0])):
+        col = []
+        for row in grid:
+          col.append(row[x])
+        height = 0
+        for y in col:
+            if(y == 0):
+                height += 1
+            else:
+                break
+        heights.append(len(col) - height)
+      d = 0
+      for i, h in enumerate(heights):
+        try:
+          if(i == 0):
+            continue
+          d += abs(h - heights[i - 1])
+        except:
           continue
-        d += abs(h - heights[i - 1])
-      except:
-        continue
+      return d
 
-    # Get height
-    height = 0 
-    for row in grid:
-      if(len(set(row)) != 1):
-        break
-      else:
-        height += 1
-    height = len(grid) - height
-
-    # Get holecount
-    holecount = 0
-
-    for x in range(len(grid[0])):
-      col = []
+    def getHeight(grid):
+      height = 0 
       for row in grid:
-        col.append(row[x])
-      changes = 0
-      first = False
-      for c in col:
-        if(first):
-          if c == 0:
-            changes += 1
-        elif(c != 0):
-          first = True
-      holecount += changes
-      
-    # Get difference between max height and other lines to avoid the spiky surface
-    hs = [] # heights
-    for x in range(len(grid[0])):
-      col = []
-      for row in grid:
-        col.append(row[x])
-      h = 0 
-      for r in col:
-        if(r != 0):
+        if(len(set(row)) != 1):
           break
         else:
-          h += 1
-      h = len(row) - h
-      hs.append(h)
+          height += 1
+      height = len(grid) - height
+      return height
 
-    dif = 0
-    m = max(hs)
-    for h in hs:
-      dif += m - h
+    def getHoleCount(grid):
+      holecount = 0
+
+      for x in range(len(grid[0])):
+        col = []
+        for row in grid:
+          col.append(row[x])
+        changes = 0
+        first = False
+        for c in col:
+          if(first):
+            if c == 0:
+              changes += 1
+          elif(c != 0):
+            first = True
+        holecount += changes
+      return holecount
+      
+    # Get difference between max height and other lines to avoid the spiky surface
+    def getMaxDiff(grid):
+      hs = [] # heights
+      for x in range(len(grid[0])):
+        col = []
+        for row in grid:
+          col.append(row[x])
+        h = 0 
+        for r in col:
+          if(r != 0):
+            break
+          else:
+            h += 1
+        h = len(row) - h
+        hs.append(h)
+
+      dif = 0
+      m = max(hs)
+      for h in hs:
+        dif += m - h
+      return dif
     
     #return (holecount, height, dif)
-    return holecount*2 + height + d/5
+    # Check that the next piece wont murder it
+    if(not onlyGrade):
+      if(self.board.checkDead(grid)):
+        return 100*(getHoleCount(grid)*3 + getHeight(grid)*(1/10) + getBoardDif(grid)/2)
+    #return (getHoleCount(grid)*2 + getBoardDif(grid)/5) / (getHeight(grid) / 20 + 1) + 2*(getHeight(grid))
+    return getHoleCount(grid)*3 + getHeight(grid)*(1/10) + getBoardDif(grid)/2
+
   def tick(self):
     if(time.time() - self.board.lastTime > self.board.fallspeed):
       move = self.getmove(self.board.piece)
       piece = move[2]
       piececoords = move[1]
-      while not self.board.checkOverlapping(piece,piececoords) and not self.board.checkInGround(piece, piececoords):
+      while not self.board.checkOverlapping(piece,piececoords,self.board.grid) and not self.board.checkInGround(piece, piececoords):
         piececoords = (piececoords[0], piececoords[1] + 1)
       piececoords = (piececoords[0], piececoords[1] - 1)
       self.board.placePiece(False, piece, piececoords)
@@ -157,7 +170,7 @@ class Board():
     # non rendered part of board
     #blocky += 2
     # seconds for piece to fall 
-    self.fallspeed = 0.5
+    self.fallspeed = 0.00000001
     self.size = (x1,y1,x2,y2)
     self.gridsize = (blockx,blocky)
     self.surface = surface
@@ -165,10 +178,9 @@ class Board():
     self.lastTime = time.time()
     self.stack = copy.deepcopy(p)
     self.piece = self.choiceRemove()
-    self.defaultPieceCoords = (((self.gridsize[0] - 1) // 2), 0)
     #self.piecestack = copy.deepcopy(p)
     #self.piece = p[1]
-    self.piececoords = list(self.defaultPieceCoords)
+    self.piececoords = self.getPieceCoords(self.piece)
     self.score = score
     self.level = 1
     self.linescleared = 0
@@ -176,6 +188,10 @@ class Board():
     self.requiredlines = 10
     self.hasAI = hasAI
   
+  # Get piece coords for initial pos of piece
+  def getPieceCoords(self,piece):
+    return [(self.gridsize[0] // 2) - (len(piece) // 2), 0]
+
   # Get piece and remove it from stack
   def choiceRemove(self):
     piece = self.stack.pop(randint(0,len(self.stack) - 1))
@@ -192,7 +208,7 @@ class Board():
             if(self.piececoords[0] + xindex > self.gridsize[0] - 1 or self.piececoords[0] + xindex < 0):
               self.piececoords = (self.piececoords[0] - xshift, self.piececoords[1])
     # Check if its overlapping
-    if(self.checkOverlapping(self.piece, self.piececoords)):
+    if(self.checkOverlapping(self.piece, self.piececoords, self.grid)):
       self.piececoords = (self.piececoords[0] - xshift, self.piececoords[1])
   
   def rotatePiece(self, rotdir, piece, returnPiece):
@@ -206,13 +222,14 @@ class Board():
         for xindex, p in enumerate(row):
           rotpiece[xindex][len(piece) - 1 - yindex] = p
     if(not returnPiece):
-      if(self.checkOverlapping(rotpiece, self.piececoords) or self.checkOut(rotpiece, self.piececoords)):
+      if(self.checkOverlapping(rotpiece, self.piececoords, self.grid) or self.checkOut(rotpiece, self.piececoords)):
         self.fitPiece(rotpiece)
         return
       else:
         self.piece = rotpiece
     else:
       return rotpiece
+
   def fitPiece(self, piece):
     MAXCHANGE = 4
     lowestChange = MAXCHANGE ** 2
@@ -222,7 +239,7 @@ class Board():
       for ychange in range(MAXCHANGE*2 + 1):
         ychange -= MAXCHANGE
         tempcoords = [self.piececoords[0] + xchange, self.piececoords[1] + ychange]
-        if(self.checkOverlapping(piece,tempcoords) or self.checkOut(piece, tempcoords)):
+        if(self.checkOverlapping(piece,tempcoords, self.grid) or self.checkOut(piece, tempcoords)):
           continue
         else:
           if(lowestChange > ((self.piececoords[0] - tempcoords[0])**2 + (self.piececoords[1] - tempcoords[1])**2)**0.5):
@@ -252,6 +269,15 @@ class Board():
           pass
       tempGrid[self.piececoords[1] + index] = r
     return tempGrid
+  
+  # Checking if placing the next piece will kill us
+  def checkDead(self, grid):
+    for piece in p:
+      for _ in range(4):
+        if(self.checkOverlapping(piece,self.getPieceCoords(piece),grid)):
+          return True
+        piece = self.rotatePiece("RIGHT",piece ,True)
+    return False
 
   def checkOut(self, piece, piececoords):
     for yindex, piecerow in enumerate(piece):
@@ -273,11 +299,11 @@ class Board():
     return inground
   
   # Check if current piece is overlapping with grid
-  def checkOverlapping(self, piece, piececoords):
+  def checkOverlapping(self, piece, piececoords, grid):
     for yindex, piecerow in enumerate(piece):
       for xindex, p in enumerate(piecerow):
         try:
-          if(self.grid[piececoords[1] + yindex][piececoords[0] + xindex] != 0 and p != 0):
+          if(grid[piececoords[1] + yindex][piececoords[0] + xindex] != 0 and p != 0):
             return True
         except IndexError:
           pass
@@ -300,10 +326,10 @@ class Board():
           continue
       g[piececoords[1] + index] = r
     if(not returnBoard):
-      self.piececoords = self.defaultPieceCoords
       self.piece = self.choiceRemove()
+      self.piececoords = self.getPieceCoords(self.piece)
       # Check for fail
-      if(self.checkOverlapping(piece=self.piece,piececoords=self.piececoords)):
+      if(self.checkOverlapping(piece=self.piece,piececoords=self.piececoords, grid=self.grid)):
         print(f"Fallspeed: {self.fallspeed} seconds per tick")
         print(f"Level: {self.level}")
         #get score object
@@ -344,8 +370,7 @@ class Board():
 
   def render(self):
     self.surface.fill((0,0,0))
-        # REMOVE THIS
-    score = pygame.font.Font('Minecraftia.ttf', 30).render("Grade: " + str(AI.evaluate(AI, self.grid)), True, (255,255,255))
+    score = pygame.font.Font('Minecraftia.ttf', 30).render("Grade: " + str(round(AI.evaluate(AI, self.grid, True),2)), True, (255,255,255))
     self.surface.blit(score, (50,750))
     self.score.render()
     tempGrid = self.overlapPiece()
@@ -374,7 +399,7 @@ class Board():
         self.placePiece(False, self.piece, self.piececoords)
       if(not inground):
         # if in piece, place piece
-        if(self.checkOverlapping(self.piece,self.piececoords)):
+        if(self.checkOverlapping(self.piece,self.piececoords, self.grid)):
           self.piececoords = (self.piececoords[0], self.piececoords[1] - 1)
           self.placePiece(False, self.piece, self.piececoords)
 
@@ -402,7 +427,7 @@ def main():
             board.rotatePiece("RIGHT", board.piece, False)
           if pygame.key.get_pressed()[pygame.K_DOWN]:
             #HARDDROP
-            while not board.checkOverlapping(board.piece,board.piececoords) and not board.checkInGround(board.piece, board.piececoords):
+            while not board.checkOverlapping(board.piece,board.piececoords, board.grid) and not board.checkInGround(board.piece, board.piececoords):
               board.piececoords = (board.piececoords[0], board.piececoords[1] + 1)
             board.piececoords = (board.piececoords[0], board.piececoords[1] - 1)
             board.placePiece(False, board.piece, board.piececoords)
